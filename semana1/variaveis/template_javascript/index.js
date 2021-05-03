a5cd7983bec17607688a0b2c587005a8d3eb012d
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-05-03 16:54:13 -0300

    exercicio-de-variaveis

---
+++ semana1/variaveis/template_javascript/index.js
@@ -0,0 +1,33 @@
+/* 
+
+1 - Será escrito no console 10 e depois 10, 10.
+
+2 - Ocorrerá algum erro pois c não está atribuido e "a" possui valor diferente de "b"
+
+3 - 
+
+*/
+
+// 1 -
+
+const nome 
+const idade 
+// Não está definido um valor para nome e idade. 
+
+console.log(typeof nome)  
+console.log(typeof idade)
+
+const nome = prompt("Qual é o seu nome")
+const idade = prompt("Qual é a sua idade?")
+
+console.log("Olá", nome, "você tem", idade, "anos")
+
+// 2 -
+
+const primeira = "você tomou água?"
+const segunda = "você come carne?"
+const terceira = "você caminha?"
+
+
+
+
