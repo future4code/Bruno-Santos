53a06a65768037d042de769d3731825e223c8721
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-05-05 16:45:04 -0300

    exercicio-de-strings-arrays

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,74 @@
+// EXERCÍCIOS
+
+// 1
+
+/*
+
+a. undefined -> Não há valor na variante
+b. null
+c. 11 -> Número de elementos
+d. 3 -> Primeira posição
+e. Mostrou todos os elementos da Array e substituiu o 4 por 19
+f. 9 -> É a 6a posição dos elementos
+
+
+// 2
+
+SUBI NUM ÔNIBUS EM MIRROCOS 27 -> Colocou todos as letras em maiusculo, trocou a vogal A por I e mostrou a quantidade de elementos.
+
+*/
+
+//1
+// const nomeDoUsuario = prompt("Qual o seu nome")
+// const emailDoUsuario = prompt("Qual o seu email")
+
+// console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso.", "Seja bem-vinda(o),", nomeDoUsuario)
+
+//2
+
+/* 
+
+a)
+const array = ['Lasanha', 'Pizza', 'Churrasco', 'Chocolate', 'Sorvete']
+console.log(array)
+
+b)
+
+const array = ['Lasanha', 'Pizza', 'Churrasco', 'Chocolate', 'Sorvete']
+console.log("Essas são as minhas comidas preferidas:", array)
+
+c)
+
+let comidaUsuario = prompt("Qual a sua comida favorita?")
+array[1] = comidaUsuario
+
+console.log("Essas são as minhas comidas preferidas:", array)
+
+// 3
+
+const array = [] 
+
+const tarefaUm = prompt("Qual a sua primeira tarefa do dia?", array)
+const tarefaDois = prompt("Qual a sua segunda tarefa do dia?", array)
+const tarefaTres = prompt("Qual a sua terceira tarefa do dia?", array)
+
+array[0] = tarefaUm
+array[1] = tarefaDois
+array[2] = tarefaTres
+
+console.log(array)  
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
