7de149041d925e5d0547d196d6358508cf652057
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-05-06 15:26:18 -0300

    exercicios-funcoes

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,9 @@
+// EXERCÍCIO 1
+// A) 10 e 50 -> Vai ser impresso no console o resultado da multiplicação entre os valores da variavel e o 5.
+// B) Não mostraria nada.
+
+// EXERCÍCIO 2
+// A) Essa função diz se a palavra inserida pelo usuário é true ou false.
+// B) i. true ii. true iii. true
+
+
