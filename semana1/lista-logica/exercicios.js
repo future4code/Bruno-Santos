269c47a4d096f9fb32bba77aebc98b42722a00cb
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-05-07 16:31:22 -0300

    exercicios-projeto-logica

---
+++ semana1/lista-logica/exercicios.js
@@ -0,0 +1,124 @@
+// Exemplos
+
+// Exercício 0A
+/*
+function soma(num1, num2) {
+   // implemente sua lógica aqui
+   return num1 + num2
+}
+
+// Exercício 0B
+
+function imprimeMensagem() {
+   // implemente sua lógica aqui
+   const mensagem = prompt('Digite uma mensagem!')
+
+   console.log(mensagem)
+}
+*/
+// Exercícios
+
+//Exercício 1
+/*
+function calculaAreaRetangulo() {
+   const altura = prompt("Qual a altura do retângulo?")
+   const largura = prompt("Qual a largura do retângulo?")
+   console.log(altura * largura)
+}
+*/
+//Exercício 2
+/*
+function imprimeIdade() {
+   const anoAtual = prompt("Em que anos estamos?")
+   const anoNascimento = prompt("Em que ano você nasceu?")
+   console.log(anoAtual - anoNascimento)
+}
+
+//Exercício 3
+*/
+/* 
+function calculaIMC(peso, altura) {
+   return peso / (altura * altura)
+}
+
+const imc = calculaIMC(85, 1.8)
+console.log(imc)
+
+
+//Exercício 4
+
+function imprimeInformacoesUsuario() {
+   // implemente sua lógica aqui
+
+}
+
+//Exercício 5
+
+function imprimeTresCoresFavoritas() {
+   const cor1 = ("Qual a sua cor favorita?")
+   const cor2 = ("Qual a sua cor favorita?")
+   const cor3 = ("Qual a sua cor favorita?")
+   const cores = (cor1, cor2, cor3)
+   console.log(cores)
+}
+
+//Exercício 6
+*/
+function retornaStringEmMaiuscula(string) {
+   return string
+}
+
+//Exercício 7
+/*
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 8
+
+function checaStringsMesmoTamanho(string1, string2) {
+   return string1 != string2
+}
+
+// Exercício 9
+
+function retornaPrimeiroElemento(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 10
+
+function retornaUltimoElemento(array) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 11
+
+function trocaPrimeiroEUltimo(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 12
+
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 13
+
+function checaRenovacaoRG() {
+   // implemente sua lógica aqui
+}
+
+// Exercício 14
+
+function checaAnoBissexto(ano) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 15
+
+function checaValidadeInscricaoLabenu(pessoa) {
+   // implemente sua lógica aqui
+}
+*/
