efe71a54f6ebba885263dc76354d1f5e03cc307f
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-05-12 16:42:36 -0300

    exercicios-loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,45 @@
+// EXERCÍCIO 1
+
+// O código está demonstrando uma constante que se inicia com o valor 0 e que vai somando 1 até chegar ao maior valor que seja menor que 5.
+// No console vai mostrar os valores: 1, 2, 3, e 4.
+
+// EXERCÍCIO 2
+
+// A) Será impresso os números maiores que 18 que estão na array.
+
+// B) Sim. 
+/*
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+
+function cadaElemento (lista) {
+    for (let numero of lista) {
+        
+	}   
+}
+
+}
+
+// EXERCÍCIO 3
+
+
+
+// EXERCÍCIO 1
+
+const quantidadePets = Number(prompt("Quantos pets você tem?"))
+
+if(quantidadePets === 0) {
+    console.log("Que pena! Você pode adotar um pet!")
+} else console.log("Qual o nome dos seus pets, um por um?")
+let pets = []
+    for(let i = 0; i < quantidadePets; i++) {
+        pets.push(prompt("Qual o nome do seu pet?"))
+}
+
+console.log(pets)
+*/
+
+// EXERCÍCIO 2
+
+let arrayOriginal = []
+
+
