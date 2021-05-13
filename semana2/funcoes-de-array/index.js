58810ab79bb1b5245571a5499eee1c5430316c40
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-05-13 16:43:47 -0300

    exercicios-callback

---
+++ semana2/funcoes-de-array/index.js
@@ -0,0 +1,35 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CODIGO
+
+// EXERCÍCIO 1
+
+// A)
+
+// EXERCÍCIO 2
+
+// A)
+
+// EXERCÍCIO 3
+
+// A)
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+// EXERCÍCIO 1
+
+// A)
+
+// B)
+
+// C)
+
+// EXERCÍCIO 2
+
+// A)
+
+// B)
+
+// C)
+
+// D)
+
+// E)
