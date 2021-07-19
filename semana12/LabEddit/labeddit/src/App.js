5b038674e852f7643d1032308f8ccc33b2b1e366
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-07-19 16:47:32 -0300

    Projeto LabEddit

---
+++ semana12/LabEddit/labeddit/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <h1>LabEddit</h1>
+    </AppContainer>
+  );
+}
+
+export default App;
