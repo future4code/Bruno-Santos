110bbe7dcc94c6eb8683f8f1c3dacd82e07e083a
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-08-09 16:44:53 -0300

    exercicios-node

---
+++ semana15/template-node-npm/exercicioUm.js
@@ -0,0 +1,11 @@
+// a) Usamos a propriedade process.argv para acessar os parâmetros passados na linha de comando para o Node.
+
+// b) e c)
+
+const nome = "Bruno";
+const idade = 29;
+const novaIdade = Number(29 + 7);
+
+console.log(
+  `Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}`
+);
