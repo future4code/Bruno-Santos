a675c6a032bb51d9bbeeefc4c8d9017f1e0de33c
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-08-15 13:29:49 -0300

    exercicios essenciais finalizados

--- semana15/LabeBank/Projeto-LabeBank/src/index.ts
+++ semana15/LabeBank/Projeto-LabeBank/src/index.ts
@@ -1,6 +1,7 @@
-import express, {Express} from "express"
+import express, {Express, Request, Response} from "express"
 import cors from "cors"
 import { AddressInfo } from "net"
+import { accounts } from "./accounts"
 
 const app: Express = express()
 
@@ -16,18 +17,77 @@ const server = app.listen(process.env.PORT || 3003, () => {
     }
 })
 
-type account = {
-    name: string,
-    CPF: number,
-    birthDate: string
-}
+// Criar conta
 
-let users: account[] = []
+app.post("/users/create", (req: Request, res: Response) => {
+    try {
 
-type bankTransfer = {
-    name: string,
-    CPF: number,
-    RcptName: string,
-    RcptCPF: number,
-    value: number
-}
+        const {name, CPF, birthDateString} = req.body 
+
+        const [day, month, year] = birthDateString.split("/")
+        
+        const birthDate: Date = new Date(`${year}-${month}-${day}`)
+
+        // validar as entradas da requisição
+
+        const ageInMilisseconds: number = Date.now() - birthDate.getTime()
+
+        const ageInYears: number = ageInMilisseconds / 1000 / 60 / 60 / 24 / 365
+
+        if (ageInYears < 18) {
+            res.statusCode = 406
+            throw new Error("Idade deve ser maior que 18 anos")
+        }
+
+        if (typeof name !== "string") {
+            throw new Error("Nome invalido")
+        }
+
+        if (typeof CPF !== "string") {
+            throw new Error("CPF invalido")
+        }
+
+        if (typeof birthDateString !== "string") {
+            throw new Error("Data de nascimento invalida")
+        }
+
+        // consultar ou alterar a base de dados  
+
+        accounts.push({
+            name,
+            CPF,
+            birthDate,
+            balance: 0,
+            statement: []
+        })
+
+        // validar os resultados da consulta
+
+        accounts.forEach(account => {
+            if (account.CPF === CPF) {
+                throw new Error("CPF já existe")
+                }
+            })
+
+        // enviar a resposta
+
+        res.status(201).send("Conta criada com sucesso!")
+
+    } catch (error) {
+        console.log(error)
+        res.send(error.message)
+    }
+})
+
+app.get("/users/all" , (req: Request, res: Response) => {
+    try {
+        if (!accounts.length) {
+            res.statusCode = 404
+            throw new Error("Nenhuma conta encontrada")
+        }
+
+        res.status(200).send(accounts)
+    } catch (error) {
+        res.send(error.message)
+    }
+})
